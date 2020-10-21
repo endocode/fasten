0337cfb6acff3f147934d1ddc97726595aaaba45
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2020-10-21 13:59:59 +0200

    Commenting the tests
    To try a new approach to mock the DB.

--- analyzer/restapi-plugin/src/test/java/eu/fasten/analyzer/restapiplugin/api/mvn/impl/BinaryModuleApiServiceImplUTest.java
+++ analyzer/restapi-plugin/src/test/java/eu/fasten/analyzer/restapiplugin/api/mvn/impl/BinaryModuleApiServiceImplUTest.java
@@ -20,7 +20,10 @@ package eu.fasten.analyzer.restapiplugin.api.mvn.impl;
 import eu.fasten.analyzer.restapiplugin.RestAPIPlugin;
 import eu.fasten.core.data.metadatadb.MetadataDao;
 
-import org.jooq.DSLContext;
+import eu.fasten.core.data.metadatadb.codegen.tables.BinaryModules;
+import eu.fasten.core.data.metadatadb.codegen.tables.PackageVersions;
+import eu.fasten.core.data.metadatadb.codegen.tables.Packages;
+import org.jooq.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,47 +40,47 @@ import static org.mockito.Mockito.*;
 
 public class BinaryModuleApiServiceImplUTest {
 
-    @Mock
-    DSLContext dslContext;
+//     @Mock
+//     DSLContext dslContext;
 
-    @Mock
-    MetadataDao kbDao;
+//     @Mock
+//     MetadataDao kbDao;
 
-    @InjectMocks
-    private final BinaryModuleApiServiceImpl service = new BinaryModuleApiServiceImpl();
-//    private final RestAPIPlugin.RestAPIExtension restExt = new RestAPIPlugin.RestAPIExtension();
+//     @InjectMocks
+//     private final BinaryModuleApiServiceImpl service = new BinaryModuleApiServiceImpl();
+// //    private final RestAPIPlugin.RestAPIExtension restExt = new RestAPIPlugin.RestAPIExtension();
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        var restExt = new RestAPIPlugin.RestAPIExtension();
-        restExt.setDBConnection(dslContext);
-    }
+//     @BeforeEach
+//     public void setUp() throws Exception {
+//         MockitoAnnotations.initMocks(this);
+//         var restExt = new RestAPIPlugin.RestAPIExtension();
+//         restExt.setDBConnection(dslContext);
+//     }
 
-    @Test
-    public void shouldCallDaoGetPkgBinaryModMethod() {
-        String pkgName = "au.org";
-        String pkgVersion = "1.1.1";
-        short offset = 1;
-        short limit = 5;
-        String resultSet = "{\"fields\":[{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"package_version_id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"name\",\"type\":\"CLOB\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"created_at\",\"type\":\"TIMESTAMP\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"metadata\",\"type\":\"JSONB\"}],\"records\":[]}\n";
-        Response response = Response.status(200).entity(resultSet).build();
-        when(service.getPackageBinaryModules(pkgName, pkgVersion, offset, limit)).thenReturn(response);
-        verify(kbDao, times(1)).getPackageBinaryModules(pkgName, pkgVersion, offset, limit);
-    }
+//     @Test
+//     public void shouldCallDaoGetPkgBinaryModMethod() {
+//         String pkgName = "au.org";
+//         String pkgVersion = "1.1.1";
+//         short offset = 1;
+//         short limit = 5;
+//         String resultSet = "{\"fields\":[{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"package_version_id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"name\",\"type\":\"CLOB\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"created_at\",\"type\":\"TIMESTAMP\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"metadata\",\"type\":\"JSONB\"}],\"records\":[]}\n";
+//         Response response = Response.status(200).entity(resultSet).build();
+//         when(service.getPackageBinaryModules(pkgName, pkgVersion, offset, limit)).thenReturn(response);
+//         verify(kbDao, times(1)).getPackageBinaryModules(pkgName, pkgVersion, offset, limit);
+//     }
 
-    @Test
-    public void shouldCallDaoGetBinaryModMetadataMethod() {
-        String pkgName = "au.org";
-        String pkgVersion = "1.1.1";
-        String binaryMod = "xx";
-        short offset = 1;
-        short limit = 5;
-        String resultSet = "{\"fields\":[{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"package_version_id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"name\",\"type\":\"CLOB\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"created_at\",\"type\":\"TIMESTAMP\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"metadata\",\"type\":\"JSONB\"}],\"records\":[]}\n";
-        Response response = Response.status(200).entity(resultSet).build();
-        when(service.getBinaryModuleMetadata(pkgName, pkgVersion, binaryMod, offset, limit)).thenReturn(response);
-        verify(kbDao, times(1)).getBinaryModuleMetadata(pkgName, pkgVersion, binaryMod, offset, limit);
-    }
+//     @Test
+//     public void shouldCallDaoGetBinaryModMetadataMethod() {
+//         String pkgName = "au.org";
+//         String pkgVersion = "1.1.1";
+//         String binaryMod = "xx";
+//         short offset = 1;
+//         short limit = 5;
+//         String resultSet = "{\"fields\":[{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"package_version_id\",\"type\":\"BIGINT\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"name\",\"type\":\"CLOB\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"created_at\",\"type\":\"TIMESTAMP\"},{\"schema\":\"public\",\"table\":\"binary_modules\",\"name\":\"metadata\",\"type\":\"JSONB\"}],\"records\":[]}\n";
+//         Response response = Response.status(200).entity(resultSet).build();
+//         when(service.getBinaryModuleMetadata(pkgName, pkgVersion, binaryMod, offset, limit)).thenReturn(response);
+//         verify(kbDao, times(1)).getBinaryModuleMetadata(pkgName, pkgVersion, binaryMod, offset, limit);
+//     }
 
 //    @Test
 //    public void shouldCallDaoGetBinaryModFilesMethod() {
